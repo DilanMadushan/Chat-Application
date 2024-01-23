33e9056a5481acac8f82676c2115ce3af2436911
Dilan Fernando <dilanmadushanka6013@gmail.com>
2024-01-23 17:18:00 +0530

    image send part is done

--- src/main/java/lk/ijse/chatApplication/Controller/ChatRoomFormController.java
+++ src/main/java/lk/ijse/chatApplication/Controller/ChatRoomFormController.java
@@ -1,6 +1,9 @@
 package lk.ijse.chatApplication.Controller;
 
+import com.sun.scenario.effect.impl.sw.java.JSWBlend_GREENPeer;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
@@ -8,6 +11,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
@@ -18,6 +22,7 @@ import java.io.DataOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.regex.Pattern;
 
 public class ChatRoomFormController {
     @FXML
@@ -35,7 +40,7 @@ public class ChatRoomFormController {
 
     @FXML
     private VBox vBox;
-    private String path;
+    private String path ="";
     private Socket socket;
     private String name;
     private DataInputStream dataInputStream;
@@ -74,23 +79,86 @@ public class ChatRoomFormController {
             try {
                 String message = dataInputStream.readUTF();
 
-//                vBox.getChildren().add(label);
-                txtDisplay.appendText(message+"\n\n");
-                System.out.println(name+" : "+message);
+                if (message.endsWith("png")||message.endsWith("jpg")){
+
+                    String[] path = message.split(" : ");
+
+                    Platform.runLater(() -> {
+
+                        ImageView imageView = new ImageView(path[1]);
+                        imageView.setStyle("-fx-padding: 10px;");
+                        imageView.setFitHeight(180);
+                        imageView.setFitWidth(100);
+
+                        HBox hBox = new HBox(imageView);
+                        hBox.setStyle("-fx-fill-height: true; -fx-min-height: 50; -fx-pref-width: 520; -fx-max-width: 520; -fx-padding: 10; -fx-alignment: center-right;");
+                        vBox.getChildren().add(hBox);
+
+                    });
+
+                }else{
+                    Platform.runLater(() -> {
+                        Label text = new Label(message);
+                        text.setStyle("-fx-background-color:   #2980b9;-fx-background-radius:15;-fx-font-size: 18;-fx-font-weight: normal;-fx-text-fill: white;-fx-wrap-text: true;-fx-alignment: center;-fx-content-display: left;-fx-padding: 10;-fx-max-width: 350;");
+                        HBox hBox = new HBox(text);
+                        hBox.setStyle("-fx-fill-height: true; -fx-min-height: 50; -fx-pref-width: 520; -fx-max-width: 520; -fx-padding: 10; -fx-alignment: center-left;");
+                        vBox.getChildren().add(hBox);
+                    });
+//
+                    System.out.println(name+" : "+message);
+
+
+                }
+
+
 
             }catch(IOException e){
-
+                throw new RuntimeException(e);
             }
         }
     }
 
     @FXML
     void sendOnAction(MouseEvent event) {
-        String message = txtMessage.getText();
-        txtMessage.clear();
         try {
-            dataOutputStream.writeUTF(name+" : "+message);
-            dataOutputStream.flush();
+            if (path==""){
+                String message = txtMessage.getText();
+                if (message==null){
+                    new Alert(Alert.AlertType.INFORMATION,"Message is empty").show();
+                }
+                txtMessage.clear();
+
+                dataOutputStream.writeUTF(name+" : "+message);
+                dataOutputStream.flush();
+
+                //create a label
+                Label label = new Label(message);
+                label.setStyle("-fx-background-color:   #F9B3A8;-fx-background-radius:15;-fx-font-size: 18;-fx-font-weight: normal;-fx-text-fill: white;-fx-wrap-text: true;-fx-alignment: center;-fx-content-display: left;-fx-padding: 10;-fx-max-width: 350;");
+
+                //create a HBox
+                HBox hBox = new HBox(label);
+                hBox.setStyle("-fx-fill-height: true; -fx-min-height: 50; -fx-pref-width: 520; -fx-max-width: 520; -fx-padding: 10; -fx-alignment: center-right;");
+
+                //set HBox in to VBox
+                vBox.getChildren().add(hBox);
+            }else {
+                dataOutputStream.writeUTF(name+" : "+path);
+                dataOutputStream.flush();
+
+                ImageView imageView = new ImageView(path);
+                imageView.setStyle("-fx-padding: 10px;");
+                imageView.setFitHeight(180);
+                imageView.setFitWidth(100);
+
+                HBox hBox = new HBox(imageView);
+                hBox.setStyle("-fx-fill-height: true; -fx-min-height: 50; -fx-pref-width: 520; -fx-max-width: 520; -fx-padding: 10; -fx-alignment: center-right;");
+                vBox.getChildren().add(hBox);
+                path="";
+
+            }
+
+
+
 
         }catch (IOException e){
 
